f3063c8c713db972c333e4b94346633e77072439
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-12 12:43:37 -0500

    Update E1.js

--- E1.js
+++ E1.js
@@ -1,63 +1,58 @@
-function sortArr(array,custom){
+function sortArr(array, custom) {
     let output;
-    switch (custom){
-        case 1:
-            console.log(`Ascending order`);
-                output = array.sort(function(a,b){
+    custom = custom.toLowerCase();
+    switch (custom) {
+        case 'ascending':
+            output = array.sort(function (a, b) {
                 let x = a.toUpperCase();
                 let y = b.toUpperCase();
                 return x == y ? 0 : x > y ? 1 : -1;
             });
             return output;
-        case 2:
-            console.log(`Descending order`);
-            output = array.sort(function (a,b){
+        case 'descending':
+            output = array.sort(function (a, b) {
                 return b.localeCompare(a);
             });
             return output;
-        case 3:
-            console.log(`Ascending order according to length`);
-            output = array.sort(function(a,b){
-             return a.length - b.length;
+        case 'ascending length':
+            output = array.sort(function (a, b) {
+                return a.length - b.length;
             });
             return output;
-        case 4:
-            console.log(`Descending order according to length`);
-            output = array.sort(function(a,b){
+        case 'descending length':
+            output = array.sort(function (a, b) {
                 return b.length - a.length;
             });
             return output;
-        case 5: 
-            console.log(`Ascending order according to number of consonants`);
+        case 'ascending consonants':
             output = array.sort(asConsonant);
             return output;
-        case 6:
-            console.log(`Descending order according to number of consonants`);
+        case 'descending consonants':
             output = array.sort(desConsonant);
-            return output; 
-        default: 
-            console.log(`The input is not allowed. Only values between 1-4 are valid`);
+            return output;
+        default:
+            throw new Error(`The input is not allowed!`);
     }
 }
 
-function isConsonant(x){
+function isConsonant(x) {
     return (/[bcdfghjklmnpqrstvwxys]/gi).test(x);
 }
 
-function asConsonant(a,b){
+function asConsonant(a, b) {
     let count1 = a.split('').filter(isConsonant).length;
     let count2 = b.split('').filter(isConsonant).length;
-    if(count1 === count2)return a.localeCompare(b)
+    if (count1 === count2) return a.localeCompare(b)
     return count1 - count2;
 }
 
-function desConsonant(a,b){
+function desConsonant(a, b) {
     let count1 = a.split('').filter(isConsonant).length;
     let count2 = b.split('').filter(isConsonant).length;
-    if(count1 === count2)return a.localeCompare(b)
+    if (count1 === count2) return a.localeCompare(b)
     return count2 - count1;
 }
 
-let array= [`viticulture`,`camel`,`butterfly`];
-let a = sortArr(array,6);
+let array = [`viticulture`, `camel`, `butterfly`];
+let a = sortArr(array, 'ascending conSonants');
 console.log(a);
