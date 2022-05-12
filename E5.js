1f9de9802e31566776ebf46f4973a037c972a15c
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-12 16:15:01 -0500

    Update E5.js

--- E5.js
+++ E5.js
@@ -1,17 +1,16 @@
-function funnyFn(param){
+function funnyFn(param) {
     let output;
-    if (isNaN(param)){
-        param = param.toLowerCase();
+    if (typeof param === "string") {
         output = param.replace(/[^aeiou]/gi, "").length;
         return output;
-        } else{
+    } else if (typeof param === "number") {
         param = parseInt(param);
-        output = Math.floor(Math.log10(param)+1);
+        output = Math.floor(Math.log10(param) + 1);
         return output;
     }
 }
 
-let a = funnyFn("2403");
+let a = funnyFn(2403);
 let b = funnyFn("Scythe");
 
 console.log(a);
