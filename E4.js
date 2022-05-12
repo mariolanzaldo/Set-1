7f42ef625d31f29adce188f20d38bea4d4e0c739
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-05-12 14:31:55 -0500

    Update E4.js

--- E4.js
+++ E4.js
@@ -1,27 +1,25 @@
-function copyProp (objA, objB,arrayIn,arrayEx){
-    if (arrayIn === undefined && arrayEx === undefined){
-      for(i in objB)
-         objA[i]= typeof objB[i]=='object' ? copyProp(objB[i]):objB[i];
-      return objA;
-    }else {
-        objA = Object.keys(objB).reduce((target, k) => {
-            if (arrayEx != undefined) {
-              if (arrayEx.indexOf(k) < 0) target[k] = objB[k];
-            } else if (arrayIn.indexOf(k) > -1) target[k] = objB[k];
-            return target;
-          }, objA);
-          return objA
-    }
+function copyProp(objA, objB, arrayIn) {
+  if (arrayIn === undefined) {
+    for (i in objB)
+      objA[i] = typeof objB[i] == 'object' ? copyProp(objB[i]) : objB[i];
+    return objA;
+  } else {
+    objA = Object.keys(objB).reduce((target, k) => {
+      if (arrayIn.indexOf(k) > -1) target[k] = objB[k];
+      return target;
+    }, objA);
+    return objA
+  }
 }
 
 let objA = {};
 let objB = {
-    p1 : "carrot",
-    p2 : "rabbit",
-    p3 : true
+  p1: "carrot",
+  p2: "rabbit",
+  p3: true
 };
 
-objA = copyProp(objA,objB,["p3","p2"]);
+objA = copyProp(objA, objB, ["p1", "p3"]);
 // objA = copyProp(objA,objB);
 console.log(objA);
 
